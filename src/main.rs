7b89305e4f2f19d9ae4d25b478a493fc57ceeda5
sh4869 <nobuk4869@gmail.com>
2017-10-02 16:34:15 +0900

    Add disqus

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,28 @@ impl Daily {
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/hopscotch.min.css"/>
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
 <script>hljs.initHighlightingOnLoad();</script>"##;
+        let disqus = r##"
+<div id="disqus_thread"></div>
+<script>
+
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+/*
+var disqus_config = function () {
+this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+*/
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://diary-sh4869-net.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                            "##;
         let title = self.day.format("%Y/%m/%d").to_string() + &" - " + &self.title;
         let markup =
             html! {
@@ -58,6 +80,8 @@ impl Daily {
                             }
                             div.daily {
                                 (PreEscaped(&self.content))
+                                hr;
+                                (PreEscaped(disqus))
                             }
                             footer {
                                 hr;
