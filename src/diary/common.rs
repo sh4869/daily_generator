60b8956016b96a5a89cc74d1ee249ba091cb8a2c
sh4869 <nobuk4869@gmail.com>
2019-06-17 00:30:55 +0900

    改修
    * sectionを使うのをやめた
    * 諸々構造を変えた

--- src/diary/common.rs
+++ src/diary/common.rs
@@ -12,11 +12,10 @@ impl Daily {
         let higlightjs = r##"<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script><script>hljs.initHighlightingOnLoad();</script>"##;
         let csslist = [
             "https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css",
-            "/static/css/layers.min.css",
-            "/static/css/layers.section.min.css",
             "/static/css/index.css",
             "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/hopscotch.min.css",
             "https://fonts.googleapis.com/css?family=Noto+Sans+JP",
+            "https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css",
         ];
         let title = self.day.format("%Y/%m/%d").to_string() + &" - " + &self.title;
         let markup = html! {
@@ -32,17 +31,40 @@ impl Daily {
                     title {(title)}
                 }
                 body{
-                    div.row {
-                        div.row-content.buffer {
-                            div.column.twelve.top#header {
-                                a href=("/") {
-                                    h1.title {"Daily Bread"}
+                    div.row {    
+                        div#header class=("col-xs-12") {
+                            a href=("/") {
+                                h1.title {"Daily Bread"}
+                            }
+                            p { "It's alright , I remember sometimes the time we chose what to bring on the journey" }
+                        }
+                    }
+                    div.row.navigation {
+                        div class=("col-xs-12 col-md-6")  {
+                            @if after.is_some() {
+                                @let link = "/".to_string() + &(after.unwrap().day.format("%Y/%m/%d").to_string()) + ".html";
+                                time.small {(after.unwrap().day.format("%Y/%m/%d"))}
+                                div.day {
+                                    a href=(link) {
+                                        p {(&after.unwrap().title)}
+                                    }
                                 }
-                                p { "It's alright , I remember sometimes the time we chose what to bring on the journey" }
                             }
-                            div.clear {
-
+                        }
+                        div class=("col-xs-12 col-md-6") {
+                            @if before.is_some() {
+                                @let link = "/".to_string() + &(before.unwrap().day.format("%Y/%m/%d").to_string()) + ".html";
+                                time.small {(before.unwrap().day.format("%Y/%m/%d"))}
+                                div.day {
+                                    a href=(link) {
+                                        p {(&before.unwrap().title)}
+                                    }
+                                }
                             }
+                        }
+                    }
+                    div.row {
+                        div class=("col-xs-12"){
                             div.info {
                                 time {(self.day.format("%Y/%m/%d"))};
                                 h1 {(self.title)};
@@ -50,39 +72,12 @@ impl Daily {
                             div.daily {
                                 (PreEscaped(&self.content))
                             }
-                            footer {
-                                hr.footer;
-                                div.row {
-                                    div.clear {
-                                    }
-                                    div.row-content {
-                                        div.column.small-full.medium-half.large-half  {
-                                            @if after.is_some() {
-                                                @let link = "/".to_string() + &(after.unwrap().day.format("%Y/%m/%d").to_string()) + ".html";
-                                                time {(after.unwrap().day.format("%Y/%m/%d"))}
-                                                div.day {
-                                                    a href=(link) {
-                                                        p {(&after.unwrap().title)}
-                                                    }
-                                                }
-                                            }
-                                        }
-                                        div.column.small-full.medium-half.medium-last {
-                                            @if before.is_some() {
-                                                @let link = "/".to_string() + &(before.unwrap().day.format("%Y/%m/%d").to_string()) + ".html";
-                                                time {(before.unwrap().day.format("%Y/%m/%d"))}
-                                                div.day {
-                                                    a href=(link) {
-                                                        p {(&before.unwrap().title)}
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                                p {(PreEscaped("&copy; 2017-2019 <a href=\"http://sh4869.net\">sh4869</a>") )}
-
-                            }
+                        }
+                    }
+                    div.row {
+                        div#footer class=("col-xs-12") {
+                            hr.footer {}
+                            p {(PreEscaped("&copy; 2017-2019 <a href=\"http://sh4869.net\">sh4869</a>") )}
                         }
                     }
                 }
--- src/diary/top_page.rs
+++ src/diary/top_page.rs
@@ -4,75 +4,89 @@ use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 
+const PER_PAGE: i32 = 60;
+
+const CSSLIST: [&str; 4] = [
+    "https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css",
+    "/static/css/index.css",
+    "https://fonts.googleapis.com/css?family=Noto+Sans+JP",
+    "https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css",
+];
+
 pub fn build_top_page(dailies: &mut Vec<Daily>) -> io::Result<()> {
     dailies.sort_by(|a, b| b.day.cmp(&a.day));
     dailies.retain(|daily| daily.title != "SKIP");
-    let csslist = [
-        "https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css",
-        "/static/css/layers.min.css",
-        "/static/css/layers.section.min.css",
-        "/static/css/index.css",
-        "https://fonts.googleapis.com/css?family=Noto+Sans+JP",
-    ];
-    let markup = html! {
-        (DOCTYPE)
-        html lang="ja" {
-            head {
-                meta chaset="utf-8";
-                meta name="viewport" content="width=device-width, initial-scale=1";
-                @for url in &csslist {
-                    link rel="stylesheet" href=(url);
+    let page_size = (dailies.len() as i32) / PER_PAGE + 1;
+    for x in 0..page_size {
+        let start = (x * PER_PAGE) as usize;
+        let end = if x == page_size - 1 {dailies.len()} else { ((x + 1) * PER_PAGE) as usize };
+        let markup = html! {
+            (DOCTYPE)
+            html lang="ja" {
+                head {
+                    meta chaset="utf-8";
+                    meta name="viewport" content="width=device-width, initial-scale=1";
+                    @for url in &CSSLIST {
+                        link rel="stylesheet" href=(url);
+                    }
+                    title {"Daily Bread"}
                 }
-                title {"Daily Bread"}
-            }
-            body {
-                div.row {
-                    div.row-content.buffer {
-                        div.column.twelve.top#header {
+                body {
+                    div.row {
+                        div#header class=("col-xs-12 top") {
                             a href=("/") {
                                 h1.title {"Daily Bread"}
                             }
                             p { "It's alright , I remember sometimes the time we chose what to bring on the journey" }
                         }
-                        div.clear {
-
-                        }
-                        @for (i,daily) in dailies.iter().enumerate() {
-                            @let link = daily.day.format("%Y/%m/%d").to_string() + ".html";
-                            @if i % 2 == 0 {
-                                div.column.small-full.medium-half.large-half {
-                                    div.day {
-                                        time {(daily.day.format("%Y/%m/%d"))};
-                                        div {
-                                            a href=(link) {
-                                                h2 {(daily.title)}
-                                            }
-                                        }
-                                    }
-                                }
-                            } @else {
-                                div.column.small-full.medium-half.medium-last {
-                                    div.day {
-                                        time {(daily.day.format("%Y/%m/%d"))};
-                                        div {
-                                            a href=(link) {
-                                                h2 {(daily.title)}
-                                            }
+                    }
+                    div.row {
+                        @for daily in dailies.as_slice()[start..end].iter() {
+                            @let link = daily.day.format("/%Y/%m/%d").to_string() + ".html";
+                            div class=("col-xs-12 col-md-6") {
+                                div.day_colum {
+                                    time {(daily.day.format("%Y/%m/%d"))};
+                                    div {
+                                        a href=(link) {
+                                            h2 {(daily.title)}
                                         }
                                     }
                                 }
                             }
                         }
-                        footer {
-                            a href=("/") {"Daily Bread"}
-                            p {(PreEscaped("&copy; 2017-2019 <a href=\"http://sh4869.net\">sh4869</a>") )}
+                    }
+                    div.row {
+                        @for y in 0..page_size {
+                            div class=("col-xs paging") {
+                                @if y != x {
+                                    @if y == 0 {
+                                        p {
+                                            a href=("/") { "1" }
+                                        }
+                                    } @else {
+                                        p {
+                                            a href=(format!("/pages/{}.html",(y+1))){ ((y+1).to_string()) }
+                                        }
+                                    }
+                                } @else {
+                                    p.current { ((y+1).to_string()) }
+                                }
+                            }
+                        }
+                    }
+                    div.row {
+                        div#footer class=("col-xs-12") {
+                            footer {
+                                p {(PreEscaped("&copy; 2017-2019 <a href=\"http://sh4869.net\">sh4869</a>") )}
+                            }
                         }
                     }
                 }
             }
-        }
-    };
-    let mut file = File::create("docs/index.html")?;
-    file.write_all(markup.into_string().as_bytes())?;
+        };
+        let filename = if x == 0 { "docs/index.html".to_string() } else { format!("docs/pages/{}.html", x + 1) };
+        let mut file = File::create(filename)?;
+        file.write_all(markup.into_string().as_bytes())?;
+    }
     Ok(())
 }
