a204bfacd18f162fa080893f696ed3a031b83a35
sh4869 <nobuk4869@gmail.com>
2020-11-25 14:47:41 +0900

    Add DiaryBuilder

---
+++ src/diary/builder.rs
@@ -0,0 +1,14 @@
+use diary::diary_page::DiaryPage;
+use std::io;
+
+pub struct BuilderOption<'a> {
+    pub dest: &'a str,
+}
+
+pub trait DiaryBuilder<'a> {
+    fn builder_name(&self) -> &'static str;
+    
+    fn new(opt: &'a BuilderOption) -> Self;
+
+    fn build(&self, diaries: &mut Vec<DiaryPage>) -> io::Result<()>;
+}
